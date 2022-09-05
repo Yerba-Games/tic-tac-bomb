1262b1ea18ed19bee405a4af81ee7f0b07a810df
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2022-09-05 15:37:43 +0200

    Dlaczego unity kurwą jest pisze ten kod od 3 godzin i nawet nie zaczołem JEBAĆ ERRORY kurwa czemu to po prostu nie działa

--- Assets/SSscripts/Player/PlayerMovement.cs
+++ Assets/SSscripts/Player/PlayerMovement.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] int classSpeed;
-
-    public void OnMove()
+    [SerializeField] float classSpeed;
+    private float inputX, inputZ;
+    private Rigidbody rb;
+    PlayerMovemnt Movemnt;
+    private void awake()
     {
+        rb = GetComponent<Rigidbody>();
+        Movemnt = new PlayerMovemnt();
+        Movemnt.Player.Move.performed += ctx => Debug.Log(ctx.ReadValueAsObject());
+    }
 
-
+    private void OnEnable()
+    {
+        Movemnt.Player.Move.Enable();
+    }
+    private void OnDisable()
+    {
+        Movemnt.Player.Move.Disable();
     }
 }
